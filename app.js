4b225494a155dbb43ce79851a9ce4a74c5ef4ee7
codeauthor <alfredkeys47@gmail.com>
2022-11-02 12:08:11 +0100

    added user model

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-(() => {
-    const express = require("express"),
-        mongoose = require("mongoose");
+const express = require("express"),
+    authRoute = require("./routes/auth"),
+    cors = require("cors"),
+    mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+app.set('view engine', "ejs")
+app.use("/user", authRoute);
+app.use(cors())
+app.use(express.json())
 
 
-    const app = express();
-    require("dotenv").config();
+mongoose.connect(
+    process.env.DB,
+    { useNewUrlParser: true },
+    () => console.log("connected to database successfully"))
 
-
-    mongoose.createConnection(
-        process.env.DB,
-        { useNewUrlParser: true },
-        () => console.log("connected to database successfully"))
-
-    app.listen(process.env.PORT, () => console.log(`server is running at port${process.env.PORT}`))
-})();
+app.listen(process.env.PORT, () => console.log(`server is running at port ${process.env.PORT}`))
